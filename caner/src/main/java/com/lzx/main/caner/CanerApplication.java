500c4bce89000c4c26fc2b0317df0a0a97505ece
刘某人 <843397092@qq.com>
2020-10-15 17:01:33 +0800

    提交

---
+++ caner/src/main/java/com/lzx/main/caner/CanerApplication.java
@@ -0,0 +1,13 @@
+package com.lzx.main.caner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CanerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CanerApplication.class, args);
+    }
+
+}
